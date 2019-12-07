36863c5198fbe5c95118307b1cb60d9cc0ab02f8
vnw1 <vnw1@javkiller.com>
2019-12-07 19:52:46 +0900

    Update infomation (front-end)

---
+++ src/public/js/updateUser.js
@@ -0,0 +1,100 @@
+let userAvatar = null;
+let userInfo = {};
+let originAvatarSrc = null;
+
+function updateUserInfo() {
+    $("#input-change-avatar").bind("change", function() {
+        let fileData = $(this).prop("files")[0];
+        let match = ["image/png", "image/jpg", "image/jpeg"];
+        let limit = 1048576; // byte = 1MB
+
+        if($.inArray(fileData.type, match) === -1) {
+            alertify.notify("Kiểu file không hợp lệ, chỉ chấp nhận jpg & png", "error", 7);
+            $(this).val(null);
+            return false;
+        }
+        if(fileData.size > limit) {
+            alertify.notify("Ảnh upload tối đa 1MB", "error", 7);
+            $(this).val(null);
+            return false;
+        }
+
+        if(typeof(FileReader) != "undefined") {
+            let imagePreview = $("#image-edit-profile");
+            imagePreview.empty();
+
+            let fileReader = new FileReader();
+            fileReader.onload = function(element) {
+                $("<img>", {
+                    "src": element.target.result,
+                    "class": "avatar img-circle",
+                    "id": "user-modal-avatar",
+                    "alt": "avatar"
+                }).appendTo(imagePreview);
+            };
+            imagePreview.show();
+            fileReader.readAsDataURL(fileData);
+
+            let formData = new FormData();
+            formData.append("avatar", fileData);
+            userAvatar = formData;
+        } else {
+            alertify.notify("Trình duyệt của bạn không hỗ trợ FileReader", "error", 7);
+        }
+    });
+
+    $("#input-change-username").bind("change", function() {
+        userInfo.username = $(this).val();
+    });
+
+    $("#input-change-gender-male").bind("click", function() {
+        userInfo.gender = $(this).val();
+    });
+
+    $("#input-change-gender-female").bind("click", function() {
+        userInfo.gender = $(this).val();
+    });
+
+    $("#input-change-address").bind("change", function() {
+        userInfo.address = $(this).val();
+    });
+
+    $("#input-change-phone").bind("change", function() {
+        userInfo.phone = $(this).val();
+    });
+};
+
+$(document).ready(function() {
+    updateUserInfo();
+
+    originAvatarSrc = $("#user-modal-avatar").attr("src");
+
+    $("#input-btn-update-user").bind("click", function() {
+        if ($.isEmptyObject(userInfo) && !userAvatar) {
+            alertify.notify("Bạn phải thay đổi thông tin trước khi lưu", "error", 7);
+            return false;
+        }
+        $.ajax({
+            url: "/user/update-avatar",
+            type: "put",
+            cache: false,
+            contentType: false,
+            processData: false,
+            data: userAvatar,
+            success: function(result) {
+                //
+            },
+            success: function(error) {
+                //
+            }
+        });
+        // console.log(userAvatar);
+        // console.log(userInfo);
+    })
+
+    $("#input-btn-cancel-update-user").bind("click", function() {
+        userAvatar = null;
+        userInfo = {};
+        $("#user-modal-avatar").attr("src", originAvatarSrc);
+    })
+});
--- src/server.js
+++ src/server.js
@@ -7,69 +7,68 @@ import connectFlash from "connect-flash";
 import configSession from "./config/session";
 import passport from "passport";
 
-import pem from "pem";
-import https from "https";
+// Init app
+let app = express();
 
-pem.createCertificate({ days: 1, selfSigned: true }, function (err, keys) {
-    if (err) {
-      throw err;
-    }
-    // Init app
-    let app = express();
+// Connect to MongoDB
+ConnectDB();
 
-    // Connect to MongoDB
-    ConnectDB();
+// Config session
+configSession(app);
 
-    // Config session
-    configSession(app);
+// Config view engine
+configViewEngine(app);
 
-    // Config view engine
-    configViewEngine(app);
+// Enable post data for request
+app.use(bodyParser.urlencoded({extended: true}));
 
-    // Enable post data for request
-    app.use(bodyParser.urlencoded({extended: true}));
+// Enable flash message
+app.use(connectFlash());
 
-    // Enable flash message
-    app.use(connectFlash());
+// Config passport js
+app.use(passport.initialize());
+app.use(passport.session());
 
-    // Config passport js
-    app.use(passport.initialize());
-    app.use(passport.session());
+// Init routes
+initRoutes(app);
 
-    // Init routes
-    initRoutes(app);
+app.listen(process.env.APP_PORT, process.env.APP_HOST, () => {
+    console.log(`hello ban ${process.env.APP_HOST}`);
+});
+
+// import pem from "pem";
+// import https from "https";
+
+// pem.createCertificate({ days: 1, selfSigned: true }, function (err, keys) {
+//     if (err) {
+//       throw err;
+//     }
+//     // Init app
+//     let app = express();
+
+//     // Connect to MongoDB
+//     ConnectDB();
+
+//     // Config session
+//     configSession(app);
+
+//     // Config view engine
+//     configViewEngine(app);
+
+//     // Enable post data for request
+//     app.use(bodyParser.urlencoded({extended: true}));
+
+//     // Enable flash message
+//     app.use(connectFlash());
+
+//     // Config passport js
+//     app.use(passport.initialize());
+//     app.use(passport.session());
+
+//     // Init routes
+//     initRoutes(app);
    
-    https.createServer({ key: keys.serviceKey, cert: keys.certificate }, app).listen(process.env.APP_PORT, process.env.APP_HOST, () => {
-        console.log(`hello ban ${process.env.APP_HOST}`);
-    });
-  });
-  
-
-// // Init app
-// let app = express();
-
-// // Connect to MongoDB
-// ConnectDB();
-
-// // Config session
-// configSession(app);
-
-// // Config view engine
-// configViewEngine(app);
-
-// // Enable post data for request
-// app.use(bodyParser.urlencoded({extended: true}));
-
-// // Enable flash message
-// app.use(connectFlash());
-
-// // Config passport js
-// app.use(passport.initialize());
-// app.use(passport.session());
-
-// // Init routes
-// initRoutes(app);
-
-// app.listen(process.env.APP_PORT, process.env.APP_HOST, () => {
-//     console.log(`hello ban ${process.env.APP_HOST}`);
-// });
+//     https.createServer({ key: keys.serviceKey, cert: keys.certificate }, app).listen(process.env.APP_PORT, process.env.APP_HOST, () => {
+//         console.log(`hello ban ${process.env.APP_HOST}`);
+//     });
+//   });
