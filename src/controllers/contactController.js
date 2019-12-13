71bb8f43537ee93e933acb4eb51fe22a06dc8800
vnw1 <vnw1@javkiller.com>
2019-12-14 00:12:51 +0900

    Add send contact request function

--- src/controllers/contactController.js
+++ src/controllers/contactController.js
@@ -25,6 +25,36 @@ let findUsersContact = async (req, res) => {
     }
 }
 
+let addNew = async (req, res) => {
+    try {
+        let currentUserId = req.user._id;
+        let contactId = req.body.uid;
+
+        let newContact = await contact.addNew(currentUserId, contactId);
+        console.log(newContact);
+        console.log(!!newContact);
+        return res.status(200).send({success: !!newContact});
+    } catch (error) {
+        return res.status(500).send(error);
+    }
+}
+
+let removeRequestContact = async (req, res) => {
+    try {
+        let currentUserId = req.user._id;
+        let contactId = req.body.uid;
+
+        let removeReq = await contact.removeRequestContact(currentUserId, contactId);
+        console.log(removeReq);
+        console.log(!!removeReq);
+        return res.status(200).send({success: !!removeReq});
+    } catch (error) {
+        return res.status(500).send(error);
+    }
+}
+
 module.exports = {
-    findUsersContact: findUsersContact
+    findUsersContact: findUsersContact,
+    addNew: addNew,
+    removeRequestContact: removeRequestContact
 };
--- src/models/contactModel.js
+++ src/models/contactModel.js
@@ -26,6 +26,38 @@ ContactSchema.statics = {
                 {"contactId": userId}
             ]
         }).exec();
+    },
+    /**
+     * Check exist of 2 users
+     * @param {string} userId 
+     * @param {string} contactId 
+     */
+    checkExists(userId, contactId) {
+        return this.findOne({
+            $or: [
+                {$and: [
+                    {"userId": userId},
+                    {"contactId": contactId}
+                ]},
+                {$and: [
+                    {"userId": contactId},
+                    {"contactId": userId}
+                ]}
+            ]
+        }).exec();
+    },
+    /**
+     * Remove request contact
+     * @param {string} userId 
+     * @param {string} contactId 
+     */
+    removeRequestContact(userId, contactId){
+        return this.deleteOne({
+            $and: [
+                {"userId": userId},
+                {"contactId": contactId}
+            ]
+        }).exec();
     }
 }
 
