7724d30dc2c5f1c56ce0d1879dd45daaad2cedb3
vnw1 <vnw1@javkiller.com>
2019-12-16 21:26:22 +0900

    render notification

---
+++ src/services/notificationService.js
@@ -0,0 +1,29 @@
+import notificationModel from "./../models/notificationModel"
+import userModel from "./../models/userModel"
+
+/**
+ * Get notification when refresh browser
+ * Just 10 notification on time
+ * @param {string} currentUserId 
+ * @param {number} limit 
+ */
+let getNotifications = (currentUserId, limit = 10) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let notifications = await notificationModel.model.getByUserIdAndLimit(currentUserId, limit);
+            
+            let getNotifContents = notifications.map(async (notification) => {
+                let sender = await userModel.findUserById(notification.senderId);
+                return notificationModel.contents.getContent(notification.type, notification.isRead, sender._id, sender.username, sender.avatar);
+            });
+
+            resolve(await Promise.all(getNotifContents));
+        } catch (error) {
+            reject(error);
+        }
+    });
+};
+
+module.exports = {
+    getNotifications: getNotifications
+};
