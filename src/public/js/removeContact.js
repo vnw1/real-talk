6607031df59977ac5ee62c120ba622069c6a6b11
vnw1 <vnw1@javkiller.com>
2020-01-24 23:21:25 +0900

    Process after approve or remove contact

--- src/public/js/removeContact.js
+++ src/public/js/removeContact.js
@@ -24,9 +24,30 @@ function removeContact() {
                     if (data.success) {
                         $("#contacts").find(`ul li[data-uid = ${targetId}]`).remove();
                         decreaseNumberNotifContact("count-contacts"); // js/calculateNotifContact.js
-                        // to be continue
                         
                         socket.emit("remove-contact", {contactId: targetId});
+
+                        // Handle chat after remove contact
+                        // Step 0: check active
+                        let checkActive = $("#all-chat").find(`li[data-chat=${targetId}]`).hasClass("active");
+
+                        // Step 1: remove leftSide.ejs
+                        $("#all-chat").find(`ul a[href="#uid_${targetId}"]`).remove();
+                        $("#user-chat").find(`ul a[href="#uid_${targetId}"]`).remove();
+
+                        // Step 2: remove rightSide.ejs
+                        $("#screen-chat").find(`div#to_${targetId}`).remove();
+
+                        // Step 3: remove image modal
+                        $("body").find(`div#imagesModal_${targetId}`).remove();
+
+                        // Step 4: remove attachment modal
+                        $("body").find(`div#attachmentsModal_${targetId}`).remove();
+
+                        // Step 5: click first conversation
+                        if (checkActive) {
+                            $("ul.people").find("a")[0].click();
+                        }
                     }
                 }
             });
@@ -37,7 +58,28 @@ function removeContact() {
 socket.on("response-remove-contact", function(user) {
     $("#contacts").find(`ul li[data-uid = ${user.id}]`).remove();
     decreaseNumberNotifContact("count-contacts"); // js/calculateNotifContact.js
-    // to be continue
+    
+    // Handle chat after remove contact
+    // Step 0: check active
+    let checkActive = $("#all-chat").find(`li[data-chat=${user.id}]`).hasClass("active");
+                        
+    // Step 1: remove leftSide.ejs
+    $("#all-chat").find(`ul a[href="#uid_${user.id}"]`).remove();
+    $("#user-chat").find(`ul a[href="#uid_${user.id}"]`).remove();
+
+    // Step 2: remove rightSide.ejs
+    $("#screen-chat").find(`div#to_${user.id}`).remove();
+
+    // Step 3: remove image modal
+    $("body").find(`div#imagesModal_${user.id}`).remove();
+
+    // Step 4: remove attachment modal
+    $("body").find(`div#attachmentsModal_${user.id}`).remove();
+
+    // Step 5: click first conversation
+    if (checkActive) {
+        $("ul.people").find("a")[0].click();
+    }
 });
 
 $(document).ready(function () {
